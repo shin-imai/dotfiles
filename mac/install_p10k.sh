aa55d2c5bffff47fbd67184c807e8ee54f76dcc2
Shin Imai <shin.imai@sedex.com>
2022-10-11 15:31:02 +0100

    Added installation for mac

---
+++ mac/install_p10k.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+### https://github.com/romkatv/powerlevel10k
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
