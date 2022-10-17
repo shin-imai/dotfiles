6986e28f8d810d51eb3d933a28a89f1588dcc0fd
Shin Imai <shin.imai@sedex.com>
2022-10-17 14:58:17 +0100

    Updated for mac

--- mac/install_ohmyzsh.sh
+++ mac/install_ohmyzsh.sh
@@ -4,4 +4,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 sed -i 's#^ZSH_THEME=.*#ZSH_THEME="powerlevel10k/powerlevel10k"#' ~/.zshrc 
-sed -i 's#^plugins=(\(.*\))#plugins=(\1 auto)#' ~/.zshrc
+sed -i 's#^plugins=(\(.*\))#plugins=(\1 zsh-autosuggestions tmux kubectl)\nZSH_TMUX_AUTOSTART=true#' ~/.zshrc
