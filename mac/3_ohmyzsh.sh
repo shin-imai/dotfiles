3dc5bc1c0a7a0a4efaa0c35af77f3509455b13ef
Shin Imai <shin.imai@sedex.com>
2022-10-20 19:30:18 +0100

    Readme updated

--- mac/3_ohmyzsh.sh
+++ mac/3_ohmyzsh.sh
@@ -5,4 +5,4 @@ curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/instal
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i "" 's#^ZSH_THEME=.*#ZSH_THEME="powerlevel10k/powerlevel10k"#' ~/.zshrc 
-sed -i "" 's#^plugins=(\(.*\))#plugins=(\1 zsh-autosuggestions tmux kubectl vi-mode)\nZSH_TMUX_AUTOSTART=false#' ~/.zshrc
+sed -i "" 's#^plugins=(\(.*\))#plugins=(\1 zsh-autosuggestions tmux kubectl vi-mode web-search)\nZSH_TMUX_AUTOSTART=false#' ~/.zshrc
--- mac/8_vscode_setup.sh
+++ mac/8_vscode_setup.sh
@@ -1,3 +1,7 @@
 #!/bin/zsh
 
-cat ~/Library/Application\ Support/Code/User/settings.json |jq '. += {"terminal.integrated.fontFamily": "FiraCode Nerd Font"}'
+tmp=mktemp
+
+cat ~/Library/Application\ Support/Code/User/settings.json |jq '. += {"terminal.integrated.fontFamily": "FiraCode Nerd Font"}' > $tmp
+cat $tmp > ~/Library/Application\ Support/Code/User/settings.json
+rm -f $tmp
