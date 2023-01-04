6086657edec300f36adf6091deee6fcae64a157a
Shin Imai <binaryninja0101@yahoo.com>
2023-01-04 09:42:32 +0000

    fetch prune defaulted

--- mac/7_git_setup.sh
+++ mac/7_git_setup.sh
@@ -6,11 +6,13 @@ git config --global difftool.vscode.cmd "code --wait --diff \$LOCAL \$REMOTE"
 git config --global difftool.prompt false
 git config --global merge.tool vimdiff
 git config --global pull.rebase true
+git config --global fetch.prune true
 git config --global core.editor nvim
 git config --global alias.d "difftool -t vimdiff"
 git config --global alias.lol "log --graph --decorate --pretty=oneline --abbrev-commit"
 git config --global alias.lola "log --graph --decorate --pretty=oneline --abbrev-commit --all"
 git config --global alias.base "merge-base HEAD origin/master"
+git config --global alias.tagrev "rev-list -n 1"
 git config --global alias.root '!pwd'
 git config --global color.branch auto
 git config --global color.diff auto
